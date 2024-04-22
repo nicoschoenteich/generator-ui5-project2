8414fe5115cc43df9e99db937fcfa7457dba0408
nicoschoenteich <nicolai.schoenteich@sap.com>
2024-04-22 15:09:51 +0200

    a lot of changes a while ago

--- generators/project/index.js
+++ generators/project/index.js
@@ -3,67 +3,69 @@ import Generator from "yeoman-generator"
 import prompts from "./prompts.js"
 
 export default class extends Generator {
-    static displayName = "Create a new OpenUI5/SAPUI5 project"
+	static displayName = "Create a new OpenUI5/SAPUI5 project"
 
-    async prompting() {
-        
-        this.answers = {}
-        await prompts.call(this)
-    }
+	async prompting() {
 
-    async writing() {
-        this.answers.projectId = `${this.answers.namespaceUI5}.${this.answers.projectName}` // e.g. com.myorg.myui5project
-        if (this.answers.newDir) {
-            this.destinationRoot(this.destinationPath(this.answers.projectId))
+		this.answers = {}
+		await prompts.call(this)
+	}
 
-            // required so that yeoman detects changes to package.json
-            // and runs install automatically if newDir === true
-            // see https://github.com/yeoman/environment/issues/309
-            // this.env.cwd = this.destinationPath()
-            // this.env.options.nodePackageManager = "npm"
-        }
-        this.config.set(this.answers) // do this after changing the directory so that .yo-src.json is created in the correct place
+	async writing() {
+		this.answers.projectId = `${this.answers.namespaceUI5}.${this.answers.projectName}` // e.g. com.myorg.myui5project
+		if (this.answers.newDir) {
+			this.destinationRoot(this.destinationPath(this.answers.projectId))
 
-        this.fs.copyTpl(
-            this.templatePath("package.json"),
-            this.destinationPath("package.json"),
-            {
-                title: this.config.get("projectId"),
-                mbtVersion: dependencies["mbt"]
-            }
-        )
-    
-        this.fs.copyTpl(
-            this.templatePath("README.md"),
-            this.destinationPath("README.md"),
-            { title: this.config.get("projectId") }
-        )
+			// required so that yeoman detects changes to package.json
+			// and runs install automatically if newDir === true
+			// see https://github.com/yeoman/environment/issues/309
+			// this.env.cwd = this.destinationPath()
+			// this.env.options.nodePackageManager = "npm"
+		}
+		this.config.set(this.answers) // do this after changing the directory so that .yo-src.json is created in the correct place
 
-        this.fs.copyTpl(
-            this.templatePath(".gitignore"),
-            this.destinationPath(".gitignore")
-        )
+		this.fs.copyTpl(
+			this.templatePath("package.json"),
+			this.destinationPath("package.json"),
+			{
+				title: this.config.get("projectId"),
+				mbtVersion: dependencies["mbt"]
+			}
+		)
 
-        this.composeWith("../uimodule/index.js", { config: this.config.getAll() })
-        this.composeWith("./platform.js", {})
-    }
+		this.fs.copyTpl(
+			this.templatePath("README.md"),
+			this.destinationPath("README.md"),
+			{ title: this.config.get("projectId") }
+		)
 
-    async end() {
-        if (this.config.get("initRepo")) {
-            this.spawnSync("git", ["init", "--quiet", "-b", "main"], {
-                cwd: this.destinationPath()
-            });
-            this.spawnSync("git", ["add", "."], {
-                cwd: this.destinationPath()
-            });
-            this.spawnSync(
-                "git",
-                ["commit", "--quiet", "--allow-empty", "-m", "Initialize repository with easy-ui5"],
-                {
-                    cwd: this.destinationPath()
-                }
-            );
-        }
-    }
+		if (this.config.get("initRepo")) {
+			this.fs.copyTpl(
+				this.templatePath(".gitignore"),
+				this.destinationPath(".gitignore")
+			)
+		}
+
+		this.composeWith("../uimodule/index.js", { config: this.config.getAll() })
+		this.composeWith("./platform.js", {})
+	}
+
+	async end() {
+		if (this.config.get("initRepo")) {
+			this.spawnSync("git", ["init", "--quiet", "-b", "main"], {
+				cwd: this.destinationPath()
+			});
+			this.spawnSync("git", ["add", "."], {
+				cwd: this.destinationPath()
+			});
+			this.spawnSync(
+				"git",
+				["commit", "--quiet", "--allow-empty", "-m", "Initialize repository with easy-ui5"],
+				{
+					cwd: this.destinationPath()
+				}
+			);
+		}
+	}
 
 }
