6d006d93f60840ebbdf75fcdc7dcc1b4afbd1086
nicoschoenteich <nicolai.schoenteich@sap.com>
2024-04-25 15:32:26 +0200

    finish view subgenerator

--- generators/view/index.js
+++ generators/view/index.js
@@ -15,118 +15,56 @@ export default class extends Generator {
 	async writing() {
 		this.log(`creating new view for ${this.options.config.uimodule}`)
 
-		// const manifestPath = `${this.options.config.uimodule}/webapp/manifest.json`
-		// const manifestJSON = JSON.parse(fs.readFileSync(this.destinationPath(manifestPath)))
+		const webappPath = `${this.options.config.uimodule}/webapp`
 
-		// const dataSource = `${this.options.config.modelName || "default"}DataSource`
+		let controllerName
+		if (this.options.config.setupController) {
+			controllerName = `controllerName="${this.options.config.uimodule}.controller.${this.options.config.viewName}"`
 
-		// let serviceUrl
-		// if (this.options.config.modelUrl) {
-		// 	serviceUrl = new URL(this.options.config.modelUrl)
-		// 	serviceUrl.pathname = serviceUrl.pathname + (serviceUrl.pathname.endsWith("/") ? "" : "/")
-		// }
+			this.fs.copyTpl(
+				this.templatePath("Controller.controller.js"),
+				this.destinationPath(`${webappPath}/controller/${this.options.config.viewName}.controller.js`),
+				{
+					uimodule: this.options.config.uimodule,
+					viewName: this.options.config.viewName
+				}
+			)
+		}
 
-		// manifestJSON["sap.ui5"]["models"][this.options.config.modelName] = {}
-		// if (!manifestJSON["sap.app"]["dataSources"]) {
-		// 	manifestJSON["sap.app"]["dataSources"] = {}
-		// }
+		this.fs.copyTpl(
+			this.templatePath("View.view.xml"),
+			this.destinationPath(`${webappPath}/view/${this.options.config.viewName}.view.xml`),
+			{
+				controller: controllerName || "",
+				viewName: this.options.config.viewName
+			}
+		)
 
-		// if (this.options.config.modelType === "JSON") {
+		if (this.options.config.setupRouteTarget) {
+			const newRoute = {
+				name: `Route${this.options.config.viewName}`,
+				pattern: this.options.config.viewName.toLowerCase(),
+				target: [
+					`Target${this.options.config.viewName}`
+				]
+			}
+			const newTarget = {
+				viewType: "XML",
+				transition: "Slide",
+				clearControlAggregation: false,
+				viewId: this.options.config.viewName,
+				viewName: this.options.config.viewName
+			}
 
-		// 	this.log(chalk.yellow("Please note: For JSON models, you have to add the data source uri in the manifest.json yourself."))
+			const manifestPath = `${this.options.config.uimodule}/webapp/manifest.json`
+			const manifestJSON = JSON.parse(fs.readFileSync(this.destinationPath(manifestPath)))
+			manifestJSON["sap.ui5"]["routing"]["routes"].push(newRoute)
+			manifestJSON["sap.ui5"]["routing"]["targets"][`Target${this.options.config.viewName}`] = newTarget
 
-		// 	manifestJSON["sap.app"]["dataSources"][dataSource] = {
-		// 		uri: "<REPLACE-WITH-URI>",
-		// 		type: "JSON",
-		// 	}
-		// 	manifestJSON["sap.ui5"]["models"][this.options.config.modelName] = {
-		// 		dataSource: dataSource,
-		// 		type: "sap.ui.model.json.JSONModel"
-		// 	}
-		// }
+			// use native yeoman methods for this subgenerator as this prompts the user before overwriting
+			this.writeDestinationJSON(this.destinationPath(manifestPath), manifestJSON, null, 4)
+		}
 
-		// if (this.options.config.modelType === "OData v2") {
-		// 	manifestJSON["sap.app"]["dataSources"][dataSource] = {
-		// 		uri: serviceUrl.pathname,
-		// 		type: "OData",
-		// 		settings: {
-		// 			annotations: [],
-		// 			localUri: "localService/metadata.xml",
-		// 			odataVersion: "2.0"
-		// 		}
-		// 	}
-		// 	manifestJSON["sap.ui5"]["models"][this.options.config.modelName] = {
-		// 		dataSource: dataSource,
-		// 		preload: true,
-		// 		settings: {}
-		// 	}
-		// }
-
-		// if (this.options.config.modelType === "OData v4") {
-		// 	manifestJSON["sap.app"]["dataSources"][dataSource] = {
-		// 		uri: serviceUrl.pathname,
-		// 		type: "OData",
-		// 		settings: {
-		// 			annotations: [],
-		// 			localUri: "localService/metadata.xml",
-		// 			odataVersion: "4.0"
-		// 		}
-		// 	}
-		// 	manifestJSON["sap.ui5"]["models"][this.options.config.modelName] = {
-		// 		dataSource: dataSource,
-		// 		preload: true,
-		// 		settings: {
-		// 			synchronizationMode: "None",
-		// 			operationMode: "Server",
-		// 			autoExpandSelect: true,
-		// 			earlyRequests: true
-		// 		}
-		// 	}
-		// }
-
-		// // use native yeoman methods for this subgenerator as this prompts the user before overwriting
-		// this.writeDestinationJSON(this.destinationPath(manifestPath), manifestJSON, null, 4)
-
-		// // set up proxy
-		// if (this.options.config.modelType.includes("OData")) {
-		// 	if (this.options.config.setupProxy) {
-		// 		const ui5YamlPath = `${this.options.config.uimodule}/ui5.yaml`
-		// 		const ui5Yaml = yaml.parse(fs.readFileSync(this.destinationPath(ui5YamlPath)).toString())
-		// 		if (!ui5Yaml.server) {
-		// 			ui5Yaml.server = {
-		// 				customMiddleware: []
-		// 			}
-		// 		}
-		// 		let proxyAlreadySetup = false
-		// 		for (let i = 0; i < ui5Yaml.server.customMiddleware.length; i++) {
-		// 			if (ui5Yaml.server.customMiddleware[i].name === "fiori-tools-proxy") {
-		// 				if (!ui5Yaml.server.customMiddleware[i].configuration.backend) {
-		// 					ui5Yaml.server.customMiddleware[i].configuration.backend = []
-		// 				}
-		// 				ui5Yaml.server.customMiddleware[i].configuration.backend.push({
-		// 					path: serviceUrl.pathname,
-		// 					url: serviceUrl.origin
-		// 				})
-		// 				proxyAlreadySetup = true
-		// 			}
-		// 		}
-		// 		if (!proxyAlreadySetup) {
-		// 			ui5Yaml.server.customMiddleware.push({
-		// 				name: "fiori-tools-proxy",
-		// 				afterMiddleware: "compression",
-		// 				configuration: {
-		// 					backend: [
-		// 						{
-		// 							path: serviceUrl.pathname,
-		// 							url: serviceUrl.origin
-		// 						}
-		// 					]
-		// 				}
-		// 			})
-		// 		}
-		// 		this.writeDestination(this.destinationPath(ui5YamlPath), yaml.stringify(ui5Yaml))
-		// 	}
-		// }
 	}
 
 }
--- generators/view/prompts.js
+++ generators/view/prompts.js
@@ -22,19 +22,18 @@ export default async function prompts() {
 				}
 			}
 			return "Please use a non-empty value and alpha numeric characters only."
-		},
-		default: ""
-	})).modelName
+		}
+	})).viewName
 
 	this.options.config.setupController = (await this.prompt({
 		type: "confirm",
-		name: "setupProxy",
+		name: "setupController",
 		message: "Do you want to set up a JavaScript controller for your new view?"
-	})).setupProxy
+	})).setupController
 
 		this.options.config.setupRouteTarget = (await this.prompt({
 		type: "confirm",
-		name: "setupRoute",
+		name: "setupRouteTarget",
 		message: "Do you want to set up a route and target for your new view?"
 	})).setupRouteTarget
 
